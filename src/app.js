3396d4089ae98624963cbd19ce8d870ebebc46e3
Eun Jin <ouhouh2222@gmail.com>
2021-05-09 13:18:21 +0900

    pug

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import path from "path";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = 7000;
+
+app.set("view engine", "pug");
+
+app.use(morgan(`dev`));
+app.use(helmet());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, "/assets")));
+
+app.listen(PORT, () => {
+  console.log(` 🐳 ${PORT} Server Start`);
+});
